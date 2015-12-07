b47bbcf2b224149d3656e6d3cf15c32140496544
Avvero <avvero@mail.ru>
2015-12-07 12:57:11 +0600

    Вывод ошибок

--- src/main/resources/static/js/directives/multiline.js
+++ src/main/resources/static/js/directives/multiline.js
@@ -1,8 +1,8 @@
-angular.module('flow').directive('multiline', function() {
+angular.module('flow').directive('multiline', function($sce) {
     return function($scope, element, attrs) {
         $scope.$watch(attrs.multiline,function(value){
             if (value.split('\n').length > 1) {
-                element.text(value.replaceAll('\n', '<br/>'));
+                element.html(value.replaceAll('\n', '<br/>'));
             } else {
                 element.text(value);
             }
